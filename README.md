a5a2d73d9545ccd8bbe7fac3710db2e7aa3eb42f
Riven0309 <2015102239@nit.edu.cn>
2021-02-14 20:36:31 +0800

    导入gtk库

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Caculator
+# Calculator
+
+c语言学习，使用gtk开发的一个计算器的项目
--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
 
-int main()
+static void print_hello(GtkWidget *widget, gpointer data)
 {
-    printf("Hello, World!\n");
-    return 0;
+    g_print("hello world\n");
+}
+
+static void activate(GtkApplication *app, gpointer user_data)
+{
+    GtkWidget  *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+
+    GtkButtonBox *buttonBox = (GtkButtonBox *) gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_container_add(GTK_CONTAINER(window), (GtkWidget *) buttonBox);
+    GtkButton *button = (GtkButton *) gtk_button_new_with_label("hello world");
+    gtk_container_add(GTK_CONTAINER(buttonBox), (GtkWidget *) button);
+    gtk_button_get_label(button);
+
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(gtk_window_close), window);
+
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char** argv)
+{
+    GtkApplication *app;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }

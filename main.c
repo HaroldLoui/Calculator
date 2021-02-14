2a37713150c3f18ffe48a5a5bfbb820852826571
Riven0309 <2015102239@nit.edu.cn>
2021-02-14 21:39:19 +0800

    使用ui布局

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-static void print_hello(GtkWidget *widget, gpointer data)
+static void print_hello(GtkWidget *widget, char const *data)
 {
-    g_print("hello world\n");
+    g_print("Hello :%s\n", data);
 }
 
-static void activate(GtkApplication *app, gpointer user_data)
-{
-    GtkWidget  *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
-
-    GtkButtonBox *buttonBox = (GtkButtonBox *) gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add(GTK_CONTAINER(window), (GtkWidget *) buttonBox);
-    GtkButton *button = (GtkButton *) gtk_button_new_with_label("hello world");
-    gtk_container_add(GTK_CONTAINER(buttonBox), (GtkWidget *) button);
-    gtk_button_get_label(button);
-
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    g_signal_connect(button, "clicked", G_CALLBACK(gtk_window_close), window);
-
-    gtk_widget_show_all(window);
-}
 
 int main(int argc, char** argv)
 {
-    GtkApplication *app;
+    GError *error = NULL;
 
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    gtk_init(&argc, &argv);
 
-    return status;
+    GtkBuilder *builder = gtk_builder_new();
+    if (gtk_builder_add_from_file(builder, "builder.ui", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    GObject *window = gtk_builder_get_object(builder, "window");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GObject *button1 = gtk_builder_get_object(builder, "button1");
+    g_signal_connect(button1, "clicked", G_CALLBACK(print_hello), "button1");
+    GObject *button2 = gtk_builder_get_object(builder, "button2");
+    g_signal_connect(button2, "clicked", G_CALLBACK(print_hello), "button2");
+    GObject *quit = gtk_builder_get_object(builder, "quit");
+    g_signal_connect(quit, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+    g_object_unref(builder);
+    gtk_main();
+
+    return 0;
 }

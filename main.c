f7326b3351a40812e4747c28147ee32aa8da2688
Riven0309 <2015102239@nit.edu.cn>
2021-02-15 20:47:55 +0800

    加载css文件

--- main.c
+++ main.c
@@ -13,15 +13,30 @@ int main(int argc, char** argv)
 
     gtk_init(&argc, &argv);
 
+    // 获取css文件
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    if (gtk_css_provider_load_from_path(css_provider, "builder.css", &error) == 0)
+    {
+        g_printerr("Error loading css file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    // 加载ui文件
     GtkBuilder *builder = gtk_builder_new();
     if (gtk_builder_add_from_file(builder, "builder.ui", &error) == 0)
     {
-        g_printerr("Error loading file: %s\n", error->message);
+        g_printerr("Error loading builder file: %s\n", error->message);
         g_clear_error(&error);
         return 1;
     }
 
     GObject *window = gtk_builder_get_object(builder, "window");
+
+    // 加载css文件
+    gtk_style_context_add_provider_for_screen(gtk_window_get_screen(GTK_WINDOW(window)),
+                                              (GtkStyleProvider *) css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
+
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     GObject *button1 = gtk_builder_get_object(builder, "button1");
